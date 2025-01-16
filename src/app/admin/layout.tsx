1f3c328ca671800417bacda90f0a734a2ebbac82
kimgh06 <kkh061101@naver.com>
2025-01-16 21:48:09 +0900

    conflict

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -15,12 +15,7 @@ export default async function Layout({ children }: ChildrenProps) {
 
   return (
     <div className="flex size-full flex-col md:p-16">
-<<<<<<< HEAD
       {permission ? children : <InsufficientPermissionScreen />}
-=======
-      {/* {permission ? children : <InsufficientPermissionScreen />} */}
-      {children}
->>>>>>> 1dfcae7686fcf538aa911f810f245e3eac47a571
     </div>
   );
 }
