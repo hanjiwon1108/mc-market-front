54538dc238a640d028f43d1c0675c43871839d52
kimgh06 <kkh061101@naver.com>
2025-02-13 21:26:26 +0900

    unstable cache

--- src/api/market/cache.ts
+++ src/api/market/cache.ts
@@ -1,8 +1,8 @@
-import { cache } from 'react';
+import { unstable_cache } from 'next/cache';
 import { getSession } from '@/api/surge';
 import { fetchMapleUser } from '@/api/market/endpoint';
 
-const mapleUserCache = cache(async () => {
+const mapleUserCache = unstable_cache(async () => {
   const session = await getSession();
   if (!session) return null;
   return await fetchMapleUser(session);
