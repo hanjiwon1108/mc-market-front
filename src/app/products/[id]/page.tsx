13af958ebb7f851dd1ec2e9c84c53ea5fbc6494e
kimgh06 <kkh061101@naver.com>
2025-03-07 09:04:32 +0900

    feature: generate metadata from products

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,6 +3,59 @@ import { MarketProductWithShortUser } from '@/api/types';
 import { getSession } from '@/api/surge';
 import { authFetch } from '@/api/surge/fetch';
 import { PageUpdater } from '@/app/products/[id]/updater';
+import { Metadata } from 'next';
+
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: PageProps) {
+  const { id } = await params;
+
+  const product = await fetch(endpoint(`/v1/products/${id}`)).then(
+    (res) => res.json() as Promise<MarketProductWithShortUser | undefined>,
+  );
+
+  if (!product) {
+    console.error('Failed to fetch product');
+    return {
+      title: '오류',
+      description: '이 페이지에 접근할 수 없습니다 ):',
+      openGraph: {
+        siteName: 'MC-Market',
+        title: '오류',
+        description: '이 페이지에 접근할 수 없습니다 ):',
+        type: 'website',
+        url: `https://mc-market.kr/articles/${id}`,
+      },
+    } as Metadata;
+  }
+
+  const title = product.name;
+  const description = `[${product.created_at}] - ${product.creator.nickname}님이 제작한 ${product.name}`;
+  const images =
+    product.details
+      .match(/<img[^>]+src="([^">]+)"/g)
+      ?.map((imgTag) => {
+        const match = imgTag.match(/src="([^">]+)"/);
+        return match ? match[1] : null;
+      })
+      .filter(Boolean) || [];
+
+  return {
+    title,
+    description,
+    openGraph: {
+      siteName: 'MC-Market',
+      title,
+      description,
+      type: 'website',
+      authors: [product.creator.nickname],
+      url: `https://mc-market.kr/articles/${id}`,
+      images,
+    },
+  } as Metadata;
+}
 
 export default async function Page({
   params,
